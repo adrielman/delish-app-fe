3b60e65e9e3fc014ae0ad8fd1853706d9228c590
Skyunarankage <premanoch@gmail.com>
2019-02-17 15:56:46 +0100

    Revert "Convert TextInput to functional component."
    This reverts commit 9701cff96f453fe1cd62fa5abc9c3ebdbfaee2f6.

--- src/app/recipe-form/TextInput.jsx
+++ src/app/recipe-form/TextInput.jsx
@@ -1,22 +1,29 @@
 import React, { Component } from "react";
 import "./TextInput.scss";
 
-const TextInput = () => {
-  const onChange = event => {
+class TextInput extends Component {
+  constructor(props) {
+    super(props);
+    this.onChange = this.onChange.bind(this);
+  }
+
+  onChange(event) {
     const { name, value } = event.target;
     this.props.onChange(name, value);
-  };
+  }
 
-  return (
-    <div className="TextInput">
-      <input
-        placeholder={this.props.label}
-        value={this.props.value}
-        name={this.props.name}
-        onChange={this.onChange}
-      />
-    </div>
-  );
-};
+  render() {
+    return (
+      <div className="TextInput">
+        <input
+          placeholder={this.props.label}
+          value={this.props.value}
+          name={this.props.name}
+          onChange={this.onChange}
+        />
+      </div>
+    );
+  }
+}
 
 export default TextInput;
