9701cff96f453fe1cd62fa5abc9c3ebdbfaee2f6
Skyunarankage <premanoch@gmail.com>
2019-02-17 15:52:54 +0100

    Convert TextInput to functional component.

--- src/app/recipe-form/TextInput.jsx
+++ src/app/recipe-form/TextInput.jsx
@@ -1,29 +1,22 @@
 import React, { Component } from "react";
 import "./TextInput.scss";
 
-class TextInput extends Component {
-  constructor(props) {
-    super(props);
-    this.onChange = this.onChange.bind(this);
-  }
-
-  onChange(event) {
+const TextInput = () => {
+  const onChange = event => {
     const { name, value } = event.target;
     this.props.onChange(name, value);
-  }
+  };
 
-  render() {
-    return (
-      <div className="TextInput">
-        <input
-          placeholder={this.props.label}
-          value={this.props.value}
-          name={this.props.name}
-          onChange={this.onChange}
-        />
-      </div>
-    );
-  }
-}
+  return (
+    <div className="TextInput">
+      <input
+        placeholder={this.props.label}
+        value={this.props.value}
+        name={this.props.name}
+        onChange={this.onChange}
+      />
+    </div>
+  );
+};
 
 export default TextInput;
