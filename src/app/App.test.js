7dc07feeff62472b1f1595f1e2e7ab1b51cdba04
Cristiana Man <man.cristiana1@gmail.com>
2019-02-17 14:18:42 +0100

    Fix test setup according to documentation on Create React App

--- src/app/App.test.js
+++ src/app/App.test.js
@@ -1,9 +1,12 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { shallow } from 'enzyme';
 import App from './App';
 
 it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+  shallow(<App />);
+});
+
+it('should match snapshot', () => {
+  const app = shallow(<App />);
+  expect(app).toMatchSnapshot();
 });
