543ba9cb4b9f2cd5712158f30092ee926d910629
Cristiana Man <man.cristiana1@gmail.com>
2019-02-19 20:59:51 +0100

    Improve styling in NumberInput

--- src/app/components/form/number-input/NumberInput.jsx
+++ src/app/components/form/number-input/NumberInput.jsx
@@ -1,13 +1,13 @@
-import React from "react";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import "./NumberInput.scss";
+import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import './NumberInput.scss';
 
 export const NumberInput = props => {
   const input = React.createRef();
 
   const onChange = event => {
     let { name, value } = event.target;
-    value = value.replace(/\D+/g, "");
+    value = value.replace(/\D+/g, '');
     props.onChange(name, Number(value));
   };
 
@@ -15,11 +15,13 @@ export const NumberInput = props => {
     input.current.focus();
     props.onChange(props.name, Number(props.value) + 1);
   };
+
   const onDecrease = () => {
     input.current.focus();
     let value = props.value === 0 ? 0 : Number(props.value) - 1;
     props.onChange(props.name, value);
   };
+
   return (
     <div className="NumberInput">
       <input
@@ -30,16 +32,12 @@ export const NumberInput = props => {
         onChange={onChange}
       />
       <div className="NumberInput-controls">
-        <FontAwesomeIcon
-          icon="angle-up"
-          className="increase"
-          onClick={onIncrease}
-        />
-        <FontAwesomeIcon
-          icon="angle-down"
-          className="decrease"
-          onClick={onDecrease}
-        />
+        <div className="increase">
+          <FontAwesomeIcon icon="angle-up" onClick={onIncrease} />
+        </div>
+        <div className="decrease">
+          <FontAwesomeIcon icon="angle-down" onClick={onDecrease} />
+        </div>
       </div>
     </div>
   );
