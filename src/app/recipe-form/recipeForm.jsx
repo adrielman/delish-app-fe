ab2641e2993afb38f85332cea6faca30f0a3cd2d
Adriel Noach Premer <premanoch@gmail.com>
2019-03-07 18:10:59 +0100

    Make recipeForm class.

---
+++ src/app/recipe-form/recipeForm.jsx
@@ -0,0 +1,43 @@
+import React, { Component } from "react";
+import "./IngredientForm.scss";
+import { Button } from "../components/button/Button.jsx";
+import { TextAreaInput } from "../components/form/text-area/TextAreaInput";
+
+class RecipeForm extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      message: ""
+    };
+
+    this.onChangeTextField = this.onChangeTextField.bind(this);
+    this.onSubmitTextField = this.onSubmitTextField.bind(this);
+  }
+
+  onChangeTextField(event) {
+    let value = event.target.value;
+    this.setState({ message: value });
+  }
+
+  onSubmitTextField() {
+    this.props.onAdd(this.state);
+  }
+
+  render() {
+    return (
+      <div className="Recipe-Form">
+        <TextAreaInput
+          onChange={this.onChangeTextField}
+          name="it's lit"
+          cols="30"
+          rows="10"
+        />
+        <div className="Recipe-Form-Button">
+          <Button onClick={this.onSubmitTextField} text={"Add"} />
+        </div>
+      </div>
+    );
+  }
+}
+export default RecipeForm;
