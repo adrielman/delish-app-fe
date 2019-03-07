c3dd5284bbe6892cbb4c927b084e4bd4a7e4bfa6
Adriel Noach Premer <premanoch@gmail.com>
2019-03-07 18:16:23 +0100

    Add routing to application.

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,17 +1,63 @@
 import React, { Component } from "react";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import "./App.scss";
 import "./Icons";
 
 import Ingredients from "./recipe-form/Ingredients";
+import RecipeWatch from "./recipe-watch/RecipeWatch";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <Ingredients />
-      </div>
+      <Router>
+        <div>
+          <nav>
+            <span>
+              <Link to="/">Home</Link>
+            </span>
+
+            <span>
+              <Link to="/">My Recipes</Link>
+            </span>
+            <span>
+              <Link to="/AllRecipes">AllRecipes</Link>
+            </span>
+            <span>
+              <Link to="/NewRecipe">New Recipe</Link>
+            </span>
+          </nav>
+          <div className="App">
+            <Route exact path="/" component={Home} />
+            <Route path="/AllRecipes" component={AllRecipes} />
+            <Route path="/NewRecipe" component={NewRecipe} />
+          </div>
+        </div>
+      </Router>
     );
   }
+}
+
+function Home() {
+  return (
+    <div>
+      <h2>Home</h2>
+    </div>
+  );
+}
+
+function AllRecipes() {
+  return (
+    <div>
+      <RecipeWatch />
+    </div>
+  );
+}
+function NewRecipe() {
+  return (
+    <div>
+      <Ingredients />
+    </div>
+  );
 }
 
 export default App;
