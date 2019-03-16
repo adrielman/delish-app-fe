38b39049ef005dd5f43261dea72a9519b0bd2433
Cristiana Man <man.cristiana1@gmail.com>
2019-03-16 22:06:57 +0100

    Style ingredient

--- src/app/recipe-form/ingredients/Ingredient.jsx
+++ src/app/recipe-form/ingredients/Ingredient.jsx
@@ -1,7 +1,10 @@
 import React from "react";
-// import styles from "./Ingredient.module.scss";
+import styles from "./Ingredient.module.scss";
+
 export const Ingredient = ({ name, amount, unit }) => (
-  <span>
-    {name} {amount} {unit}
-  </span>
+  <div className={styles.ingredient}>
+    <div className={styles.amount}>{amount}</div>
+    <div className={styles.unit}>{unit}</div>
+    <div className={styles.name}>{name}</div>
+  </div>
 );
