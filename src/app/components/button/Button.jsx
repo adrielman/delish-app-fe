4716e45f22753626899895245af80d4c14f0eb1a
Cristiana Man <man.cristiana1@gmail.com>
2019-03-16 23:33:53 +0100

    Add compact style to button.

--- src/app/components/button/Button.jsx
+++ src/app/components/button/Button.jsx
@@ -1,13 +1,15 @@
 import React from "react";
 import styles from "./Button.module.scss";
+import classNames from "classnames";
 
-export const Button = ({ onClick, children }) => {
+export const Button = ({ onClick, children, compact = false }) => {
   const clickHandler = event => {
     event.preventDefault();
     onClick();
   };
+  let buttonStyle = classNames(styles.button, { [styles.compact]: compact });
   return (
-    <button className={styles.button} onClick={clickHandler}>
+    <button className={buttonStyle} onClick={clickHandler}>
       {children}
     </button>
   );
