ec6a2f84319f429d809e3afa1e08ecf52f5e41ff
Adriel Noach Premer <premanoch@gmail.com>
2019-03-26 21:40:52 +0100

    Remove clutter from RecipeApi.

--- src/app/recipe-form/RecipeApi.js
+++ src/app/recipe-form/RecipeApi.js
@@ -4,39 +4,16 @@ const api = axios.create({
   baseURL: "https://delish-recipe-api.herokuapp.com/api"
 });
 
-export async function deleteRecipe(id) {
+export const deleteRecipe = async id => {
   await api.delete(`recipes/${id}`);
-}
+};
 
-export async function postRecipe(recipe) {
+export const postRecipe = async recipe => {
   await api.post("recipes", recipe);
-}
+};
 
-// onSubmit() {
-//   let date = new Date();
-
-//   let output = {
-//     name: this.state.name,
-//     description: this.state.description,
-//     image: this.state.imgUrl,
-//     time: date.getTime(),
-//     servings: this.state.servings,
-//     link: this.state.source,
-//     ingredients: this.state.ingredients,
-//     instructions: this.state.recipes
-//   };
-//   fetch("https://delish-recipe-api.herokuapp.com/api/recipes", {
-//     method: "POST",
-//     mode: "cors",
-//     cache: "no-cache",
-//     credentials: "same-origin",
-//     headers: {
-//       "Content-Type": "application/json"
-//     },
-//     redirect: "follow",
-//     referrer: "no-referrer",
-//     body: JSON.stringify(output)
-//   }).then(response => response.json()); // parses response to JSON
-
-//   console.log(JSON.stringify(output));
-// }
+export const getRecipes = async () => {
+  const response = await api.get("recipes");
+  console.log("getRecipes", response);
+  return response.data;
+};
