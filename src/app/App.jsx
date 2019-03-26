78c067122533736c9fd10b3c4f091652e13ddc12
Adriel Noach Premer <premanoch@gmail.com>
2019-03-26 21:41:16 +0100

    Add store to App

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,31 +1,41 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import styles from "./App.module.scss";
 import "./Icons";
+import Store, { onUpdate } from "../store/store";
 
 import { Navigation, Link } from "./components/navigation/Navigation";
-import Recipes from "./recipes/Recipes-Watch";
+import RecipePage from "./recipes/RecipePage";
 import { NewRecipe } from "./pages/NewRecipe";
 
 export const App = () => {
-    return (
-      <Router>
-        <div>
-          <Navigation>
-            <Link to="/recipes" exact>
-              Recipes
-            </Link>
-            <Link to="/new-recipe">New Recipe</Link>
-          </Navigation>
-          <div className={styles.app}>
-            <Route exact path="/" component={NewRecipe} />
-            <Route path="/new-recipe" component={NewRecipe} />
-            <Route path="/recipes" component={Recipes} />
-          </div>
+  const [recipes, setRecipes] = useState([]);
+  const [error, setError] = useState([]);
+
+  const Update = () => {
+    onUpdate(setRecipes);
+  };
+  Update();
+
+  return (
+    <Router>
+      <Store.Provider
+        value={{ recipes: recipes, OnUpdate: Update, error: error }}
+      >
+        <Navigation>
+          <Link to="/recipes" exact>
+            Recipes
+          </Link>
+          <Link to="/new-recipe">New Recipe</Link>
+        </Navigation>
+        <div className={styles.app}>
+          <Route exact path="/" component={NewRecipe} />
+          <Route path="/new-recipe" component={NewRecipe} />
+          <Route path="/recipes" component={RecipePage} />
         </div>
-      </Router>
-    );
-  }
-}
+      </Store.Provider>
+    </Router>
+  );
+};
 
 export default App;
