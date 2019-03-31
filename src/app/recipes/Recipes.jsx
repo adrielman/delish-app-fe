91d68b01ca58ec3725c546437932888f6fa99164
Adriel Noach Premer <premanoch@gmail.com>
2019-04-01 00:21:19 +0200

    Add masonry shared style.

--- src/app/recipes/Recipes.jsx
+++ src/app/recipes/Recipes.jsx
@@ -1,9 +1,10 @@
 import React from "react";
 import { Recipe } from "./Recipe";
+import "../components/masonry-layout/MasonryStandardStyle.css";
 
 export const Recipes = ({ recipes, onSelect }) =>
   recipes.map((message, index) => (
-    <div class="grid-item" key={index}>
+    <div className="grid-item" key={index}>
       <Recipe onSelect={onSelect} index={index} recipe={message} />
     </div>
   ));
