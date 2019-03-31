2bedc584ce5bd910563643558171c3922fd14985
Cristiana Man <man.cristiana1@gmail.com>
2019-03-31 16:57:26 +0200

    Modify NewRecipe component to use recipe context.

--- src/app/pages/NewRecipe.jsx
+++ src/app/pages/NewRecipe.jsx
@@ -1,28 +1,13 @@
 import React from "react";
 import { RecipeEditor } from "../recipe-form/RecipeEditor";
-
-import { createRecipe } from "../RecipeApi";
-
+import { RecipeConsumer } from "../RecipeContext";
 import { Layout } from "../components/layout/Layout";
 export const NewRecipe = () => {
-  const onSubmit = ({ title, image, directions, ingredients }) => {
-    const date = new Date();
-
-    const recipe = {
-      name: title,
-      description: "this.state.description",
-      image: image,
-      time: date.getTime(),
-      servings: 100,
-      link: "this.state.source",
-      ingredients: ingredients,
-      instructions: directions
-    };
-    createRecipe(recipe);
-  };
   return (
     <Layout width={500}>
-      <RecipeEditor onSubmit={onSubmit} />
+      <RecipeConsumer>
+        {({ onCreate }) => <RecipeEditor onSubmit={onCreate} />}
+      </RecipeConsumer>
     </Layout>
   );
 };
