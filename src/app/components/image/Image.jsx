1ec87cb2bff2d37d096a46d2bbf0dbd1df840494
Cristiana Man <man.cristiana1@gmail.com>
2019-04-02 19:56:22 +0200

    Cleanup Image

--- src/app/components/image/Image.jsx
+++ src/app/components/image/Image.jsx
@@ -1,7 +1,5 @@
 import React from "react";
 import styles from "./Image.module.scss";
 
-export const Image = ({ src, alt }) => {
-  if (src === "") src = "https://i.ytimg.com/vi/WRJ-1O-mjKg/maxresdefault.jpg";
-  return <img src={src} alt={alt} className={styles.image} />;
-};
+export const Image = ({ src, alt }) =>
+  src ? <img src={src} alt={alt} className={styles.image} /> : null;
