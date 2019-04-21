660d7e3a46521a6066f27b3abe5b0247cb9e19ce
Adriel Noach Premer <premanoch@gmail.com>
2019-04-21 18:01:24 +0200

    Fix Ingredient display

--- src/app/recipe-editor/ingredients/Ingredient.jsx
+++ src/app/recipe-editor/ingredients/Ingredient.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 import styles from "./Ingredient.module.scss";
 
-export const Ingredient = ({ name, amount, unit }) => (
+export const Ingredient = ({ name, quantity, unit }) => (
   <div className={styles.ingredient}>
-    <div className={styles.amount}>{amount}</div>
+    <div className={styles.quantity}>{quantity}</div>
     <div className={styles.unit}>{unit}</div>
     <div className={styles.name}>{name}</div>
   </div>
