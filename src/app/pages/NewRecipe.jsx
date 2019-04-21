3a20715927e6d441becc9eb023b7e408461ea02c
Adriel Noach Premer <premanoch@gmail.com>
2019-04-21 17:38:45 +0200

    Rename recipe-form to recipe-editor

--- src/app/pages/NewRecipe.jsx
+++ src/app/pages/NewRecipe.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { RecipeEditor } from "../recipe-form/RecipeEditor";
+import { RecipeEditor } from "../recipe-editor/RecipeEditor";
 import { RecipeConsumer } from "../RecipeContext";
 import { Layout } from "../components/layout/Layout";
 export const NewRecipe = () => {
