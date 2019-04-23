0a4d19dbc8a8fd0688fe0f75440d19598b1d97b4
Adriel Noach Premer <premanoch@gmail.com>
2019-04-23 21:10:15 +0200

    Recreate RecipePage as functional component.

--- src/app/recipes/RecipesPage.jsx
+++ src/app/recipes/RecipesPage.jsx
@@ -1,47 +1,23 @@
-import React, { Component } from "react";
+import React from "react";
 import { Recipes } from "./Recipes";
 import { RecipeExpanded } from "./RecipeExpanded";
 import { RecipeConsumer } from "../RecipeContext";
-const NONE = null;
-export class RecipesPage extends Component {
-  constructor(props) {
-    super(props);
-    this.state = { selectedRecipe: NONE };
-  }
 
-  closeRecipe() {
-    this.setState({ selectedRecipe: NONE });
-  }
-
-  selectRecipe = recipe => {
-    this.setState({ selectedRecipe: recipe });
-  };
-
-  render() {
-    const { selectedRecipe } = this.state;
-    const isRecipeSelected = !!selectedRecipe;
-    if (isRecipeSelected) {
-      console.log("SELECTED RECIPE", selectedRecipe);
-    }
-
-    return (
-      <RecipeConsumer>
-        {({ recipes, onEdit, onDelete }) =>
-          isRecipeSelected ? (
-            <RecipeExpanded
-              recipe={selectedRecipe}
-              onEdit={onEdit}
-              onDelete={() => {
-                onDelete(selectedRecipe._id);
-                this.closeRecipe();
-              }}
-              onClose={() => this.closeRecipe()}
-            />
-          ) : (
-            <Recipes onSelect={this.selectRecipe} recipes={recipes} />
-          )
-        }
-      </RecipeConsumer>
-    );
-  }
-}
+export const RecipesPage = () => {
+  return (
+    <RecipeConsumer>
+      {({ recipes, selectedRecipe, onEdit, onDelete, onClose, onSelect }) =>
+        !!selectedRecipe ? (
+          <RecipeExpanded
+            recipe={selectedRecipe}
+            onEdit={onEdit}
+            onDelete={onDelete}
+            onClose={onClose}
+          />
+        ) : (
+          <Recipes onSelect={onSelect} recipes={recipes} />
+        )
+      }
+    </RecipeConsumer>
+  );
+};
