daf09639f8c668149be7f27c18775d901de44b59
Skyunarankage <premanoch@gmail.com>
2019-05-11 17:59:31 +0200

    Redirect after editing recipe or creating recipe..

--- src/app/recipes/RecipeEditorPage.jsx
+++ src/app/recipes/RecipeEditorPage.jsx
@@ -2,16 +2,24 @@ import React from "react";
 import { RecipeEditor } from "../components/recipe-editor/RecipeEditor";
 import { RecipeConsumer } from "../RecipeContext";
 import { Layout } from "../components/layout/Layout";
-export const RecipeEditorPage = ({ match }) => {
+export const RecipeEditorPage = ({ match,history }) => {
+
+  
   const isNew = match.params.id === "new";
+
+  const onSubmit = (updateRecipeConsumer,recipe) =>{
+    updateRecipeConsumer(recipe);
+    history.push('/');
+  }
+  
   return (
     <Layout width={500}>
       <RecipeConsumer>
         {({ onCreate, onUpdate, selectedRecipe }) =>
           isNew ? (
-            <RecipeEditor onSubmit={onCreate} />
+            <RecipeEditor onSubmit={recipe => onSubmit(onCreate,recipe)} />
           ) : (
-            <RecipeEditor recipe={selectedRecipe} onSubmit={onUpdate} />
+            <RecipeEditor recipe={selectedRecipe} onSubmit={recipe => onSubmit(onUpdate,recipe)} />
           )
         }
       </RecipeConsumer>
